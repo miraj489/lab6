6be2f994f1900d3ace70dbd2d20f3d6af58b3fec
rahman15-4893 <rahman15-4893@diu.edu.bd>
2025-02-22 04:33:36 +0000

    lab06

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: Text("lab 06")),
+        body: Center(
+          child: Image.network(
+            'https://diablodesign.eu/images/DiabloDesign/blog/online-photo-editor/v2-polarr.webp', 
+            loadingBuilder: (context, child, loadingProgress) {
+              if (loadingProgress == null) {
+                return child;
+              } else {
+                return Center(child: CircularProgressIndicator());
+              }
+            },
+            errorBuilder: (context, error, stackTrace) {
+              return Icon(Icons.error, size: 50, color: Colors.red);
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
