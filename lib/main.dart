666f84201e6adec663dda431d289c1b4a4aba258
rahman15-4893 <rahman15-4893@diu.edu.bd>
2025-02-22 05:49:22 +0000

    image loaded

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart'; // Add this import
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'Lab 6 work',
       home: Scaffold(
-        appBar: AppBar(title: Text("lab 06")),
-        body: Center( 
-          child: Image.network(
-            'https://diablodesign.eu/images/DiabloDesign/blog/online-photo-editor/v2-polarr.webp', 
-            loadingBuilder: (context, child, loadingProgress) {
-              if (loadingProgress == null) {
-                return child;
-              } else {
-                return Center(child: CircularProgressIndicator());
-              }
-            },
-            errorBuilder: (context, error, stackTrace) {
-              return Icon(Icons.error, size: 50, color: Colors.red);
-            },
-          ),
+        appBar: AppBar(
+          title: const Text('Lab 6 work'), 
+          backgroundColor: const Color.fromARGB(255, 228, 16, 9), 
+        ),
+        backgroundColor: const Color(0xFFE3E8FF),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded( 
+              child: Align(
+                alignment: Alignment.center, 
+                child: CachedNetworkImage(
+                  imageUrl: "https://diablodesign.eu/images/DiabloDesign/blog/online-photo-editor/v2-polarr.webp",
+                  placeholder: (context, url) => const CircularProgressIndicator(), 
+                  errorWidget: (context, url, error) => const Icon(Icons.error), 
+                  fit: BoxFit.contain, 
+                ),
+              ),
+            ),
+            const SizedBox(height: 20), 
+            Expanded( 
+              child: Align(
+                alignment: Alignment.center, 
+                child: CachedNetworkImage(
+                  imageUrl: "https://static1.cbrimages.com/wordpress/wp-content/uploads/2024/03/boa-hancock-talks-excitedly-while-blushing.jpg",
+                  placeholder: (context, url) => const CircularProgressIndicator(), 
+                  errorWidget: (context, url, error) => const Icon(Icons.error),
+                  
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
